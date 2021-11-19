cba5f3547d1d4a0e8a8abbf477a7b576cb559715
921-Bican-Iulia <72073472+921-Bican-Iulia@users.noreply.github.com>
2021-11-19 18:32:20 +0200

    Grammar Class - first iteration

---
+++ Lab5/src/Main.java
@@ -0,0 +1,146 @@
+
+import model.CustomScanner;
+import model.FiniteAutomata;
+import model.Grammar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+//    public static void main(String[] args) throws IOException {
+//        FiniteAutomata FA = new FiniteAutomata("D:/YearIII/FLCD/Labs/Formal-Languages-and-Compiler-Design/Lab4/src/fa.txt");
+//        FA.readFromFile();
+//
+//        while (true) {
+//            display_menu();
+//            String command = "";
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//            System.out.println("Enter command: ");
+//            command = reader.readLine();
+//
+//            switch (command) {
+//                case "1":
+//                    System.out.println("States: ");
+//                    System.out.println(FA.getSetOfStates());
+//                    System.out.println("\n");
+//                    break;
+//                case "2":
+//                    System.out.println("Alphabet: ");
+//                    System.out.println(FA.getAlphabet());
+//                    System.out.println("\n");
+//                    break;
+//                case "3":
+//                    System.out.println("Transitions: ");
+//                    System.out.println(FA.getTransitionsList());
+//                    System.out.println("\n");
+//                    break;
+//                case "4":
+//                    System.out.println("Final states: ");
+//                    System.out.println(FA.getFinalStates());
+//                    System.out.println("\n");
+//                    break;
+//                case "5":
+//                    String message = FA.isDFA() ? "is a DFA" : "is NOT a DFA";
+//                    System.out.println("The introduced FA " + message);
+//                    System.out.println("\n");
+//                    break;
+//                case "6":
+//                    if(!FA.isDFA()){
+//                        System.out.println("FA needs to be a DFA.");
+//                        break;
+//                    }
+//                    BufferedReader reader2 =  new BufferedReader(new InputStreamReader(System.in));
+//                    System.out.println("Enter sequence: ");
+//                    String sequence = reader2.readLine();
+//                    String message2 = FA.isAccepted(sequence) ? "Sequence is accepted" : "Sequence is not accepted";
+//                    System.out.println(message2);
+//                    System.out.println("\n");
+//                    break;
+//                case "0":
+//                    System.exit(0);
+//                default:
+//                    System.err.println("Unrecognized option");
+//                    break;
+//            }
+//        }
+//    }
+//
+//    private static void display_menu() {
+//        System.out.println("1 - Show states");
+//        System.out.println("2 - Show alphabet");
+//        System.out.println("3 - Show transitions");
+//        System.out.println("4 - Show final states");
+//        System.out.println("5 - Is DFA?");
+//        System.out.println("6 - Verify if a sequence is accepted by FA");
+//        System.out.println("0 - Exit \n");
+//    }
+
+
+//    public static void main(String[] args) throws IOException {
+//        CustomScanner scanner = new CustomScanner("programs/p1");
+//        scanner.scan();
+//
+//        // PIF
+//        scanner.classifyTokens();
+//
+//        // ST
+//        scanner.writeToSymbolTable();
+//    }
+
+    private static void display_menu() {
+        System.out.println("1 - Show non-terminals");
+        System.out.println("2 - Show terminals");
+        System.out.println("3 - Show productions");
+        System.out.println("4 - Show starting symbol");
+        System.out.println("5 - Production for given non-terminal");
+        System.out.println("0 - Exit \n");
+    }
+
+    public static void main(String[] args) throws IOException {
+        Grammar grammar = new Grammar("D:/YearIII/FLCD/Labs/Formal-Languages-and-Compiler-Design/Lab5/src/data/g1.txt");
+        grammar.readFromFile();
+
+        while (true) {
+            display_menu();
+            String command = "";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter command: ");
+            command = reader.readLine();
+
+            switch (command) {
+                case "1":
+                    System.out.println("Non-terminals: ");
+                    System.out.println(grammar.getSetOfNonTerminals());
+                    System.out.println("\n");
+                    break;
+                case "2":
+                    System.out.println("Terminals: ");
+                    System.out.println(grammar.getSetOfTerminals());
+                    System.out.println("\n");
+                    break;
+                case "3":
+                    System.out.println("Productions: ");
+                    System.out.println(grammar.getSetOfProductions());
+                    System.out.println("\n");
+                    break;
+                case "4":
+                    System.out.println("Starting symbol: ");
+                    System.out.println(grammar.getStartingSymbol());
+                    System.out.println("\n");
+                    break;
+                case "5":
+                    //TODO Check CFG
+                    break;
+                case "0":
+                    System.exit(0);
+                default:
+                    System.err.println("Unrecognized option");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Lab5/src/model/CustomScanner.java
@@ -0,0 +1,373 @@
+package model;
+
+import javafx.util.Pair;
+
+import java.io.*;
+import java.util.*;
+
+
+
+public class CustomScanner {
+    private String fileName;
+    private boolean isStringLexicallyCorrect = true;
+    private boolean isCharLexicallyCorrect = true;
+    private List<String> tokenList = new ArrayList<String>();
+    private List<String> separatorList = new ArrayList<String>();
+    private List<Pair<String, Integer>> detectedTokens = new ArrayList<>();
+    private Map<String, Integer> PIF = new HashMap<String, Integer>();
+    private Map<Integer, String> ST = new HashMap<Integer, String>();
+    private List<String> specialRelational = new ArrayList<String>();
+    private List<String> regularRelational = new ArrayList<String>();
+    private String stringConstant = "";
+    private String charConstant = "";
+    private int currentLine = 0;
+    private int capacity = 97;
+    private CustomHashTable hashTable = new CustomHashTable(capacity);
+
+    public CustomScanner(String fileName) {
+        this.fileName = fileName;
+        this.specialRelational.add(">=");
+        this.specialRelational.add("<=");
+        this.specialRelational.add("==");
+        this.regularRelational.add(">");
+        this.regularRelational.add("<");
+        this.readTokens();
+        this.readSeparators();
+    }
+
+    private void readTokens() {
+        try {
+            File myObj = new File("scanner_input/token");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                tokenList.add(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    private void readSeparators() {
+        try {
+            File myObj = new File("scanner_input/token");
+            Scanner myReader = new Scanner(myObj);
+            for (int i = 0; i < 28; i++){
+                String data = myReader.nextLine();
+                separatorList.add(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    private Boolean isConstant(String token) throws FileNotFoundException {
+        FiniteAutomata FA_constant = new FiniteAutomata("D:/YearIII/FLCD/Labs/Formal-Languages-and-Compiler-Design/Lab3/fa/constant");
+        FA_constant.readFromFile();
+        if(!FA_constant.isDFA()) {
+            return false;
+        }
+
+        FiniteAutomata FA_identifier = new FiniteAutomata("D:/YearIII/FLCD/Labs/Formal-Languages-and-Compiler-Design/Lab3/fa/identifier");
+        FA_identifier.readFromFile();
+        if(!FA_identifier.isDFA()){
+            return false;
+        }
+        return FA_constant.isAccepted(token) || FA_identifier.isAccepted(token);
+    }
+
+    private Boolean isIdentifier(String token) throws FileNotFoundException {
+        FiniteAutomata FA_identifier = new FiniteAutomata("D:/YearIII/FLCD/Labs/Formal-Languages-and-Compiler-Design/Lab3/fa/identifier");
+        FA_identifier.readFromFile();
+        if(!FA_identifier.isDFA()){
+            return false;
+        }
+        return FA_identifier.isAccepted(token);
+    }
+
+    private Boolean isStringConstant(String token) {
+        if (token.charAt(0) == '"' && token.charAt(token.length() - 2) == '"') {
+            String withoutQuote = token.substring(1, token.length() - 2);
+            return withoutQuote.length() > 1;
+        } else {
+            return false;
+        }
+    }
+
+    private Boolean isCharConstant(String token) {
+        if (String.valueOf(token.charAt(0)).equals("'") && String.valueOf(token.charAt(token.length() - 2)).equals("'")) {
+            String withoutQuote = token.substring(1, token.length() - 2);
+            return withoutQuote.length() <= 1;
+        } else {
+            return false;
+        }
+    }
+
+    private Boolean isReservedOperatorSeparator(String myToken) {
+        for (String token : this.tokenList) {
+            if (myToken.equals(token)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void classifyTokens() throws IOException {
+        // Pair is token + line of token
+        PrintWriter pw = new PrintWriter("D:/YearIII/FLCD/Labs/Formal-Languages-and-Compiler-Design/Lab3/scanner_output/pif");
+        pw.printf("%-20s %s\n", "Token", "ST_Pos");
+
+        Integer lastLine = 0;
+        for (Pair<String, Integer> pair: this.detectedTokens) {
+            if (isReservedOperatorSeparator(pair.getKey())) {
+                pw.printf("%-20s %d\n", pair.getKey(), -1);
+            } else if (isIdentifier(pair.getKey()) || isConstant(pair.getKey())
+                    || isStringConstant(pair.getKey()) || isCharConstant(pair.getKey())) {
+                // index is the position from the ST
+
+                hashTable.insert(pair.getKey());
+                int position = hashTable.find(pair.getKey());
+                pw.printf("%-20s %d\n", pair.getKey(), position);
+            } else {
+                System.out.println("LEXICAL ERROR " + pair.getKey() + " AT LINE " + (pair.getValue()));
+            }
+            lastLine = pair.getValue();
+        }
+
+        if (!isStringLexicallyCorrect) {
+            System.out.println("LEXICAL ERROR: DOUBLE QUOTES NOT CLOSED AT LINE " + lastLine);
+        }
+        if (!isCharLexicallyCorrect) {
+            System.out.println("LEXICAL ERROR: SINGLE QUOTES NOT CLOSED AT LINE " + lastLine);
+        }
+        pw.flush();
+    }
+
+    public void writeToSymbolTable() throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter("D:/YearIII/FLCD/Labs/Formal-Languages-and-Compiler-Design/Lab3/scanner_output/st");
+        pw.printf("%-20s %s\n", "Symbol Table as:", "Hash Table");
+        pw.printf("%-20s %s\n", "Symbol", "ST_Pos");
+        String[] symTable = hashTable.getSymTable();
+
+        for(int i = 0; i < capacity; i++) {
+            if (symTable[i] != null) {
+                pw.printf("%-20s %s\n", symTable[i], i);
+            }
+        }
+        pw.flush();
+    }
+
+    public void scan() {
+        try {
+            File myObj = new File(this.fileName);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                Scanner data = new Scanner(myReader.nextLine());
+                currentLine ++;
+                while (data.hasNext()) {
+                    String word = data.next();
+                    boolean hasSeparator = false;
+
+                    for (String separator : separatorList) {
+                        // General separator cases
+                        if (word.contains(separator)) {
+                            hasSeparator = true;
+                            this.splitWordWithSeparator(word, separator, currentLine);
+                            break;
+                        }
+                    }
+
+                    if (!hasSeparator && !isStringLexicallyCorrect) {
+                        stringConstant += word + " ";
+                    }
+                    if (!hasSeparator && !isCharLexicallyCorrect) {
+                        charConstant += word + " ";
+                    }
+                    if (!hasSeparator && isStringLexicallyCorrect && isCharLexicallyCorrect) {
+                        detectedTokens.add(new Pair<String, Integer>(word, currentLine));
+                    }
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    private void splitWordWithSeparator(String word, String separator, Integer line) {
+        String[] splitList;
+
+        boolean specialCase = false;
+        boolean containsRelational = false;
+        char doubleQuotes = '"';
+        String stringDoubleQuotes = String.valueOf(doubleQuotes);
+
+        // String part
+        if (word.contains(stringDoubleQuotes) && !isStringLexicallyCorrect) {
+            specialCase = true;
+
+            this.isStringLexicallyCorrect = true;
+            if (word.charAt(word.length() - 1) == ';') {
+                String newWord = word.substring(0, word.length() - 1);
+                stringConstant += newWord + " ";
+                detectedTokens.add(new Pair<String, Integer>(stringConstant, currentLine));
+                detectedTokens.add(new Pair<String, Integer>(";", currentLine));
+                stringConstant = "";
+                return;
+            } else {
+                stringConstant += word + " ";
+                detectedTokens.add(new Pair<String, Integer>(stringConstant, currentLine));
+                stringConstant = "";
+                return;
+            }
+        }
+
+        if (!isStringLexicallyCorrect) {
+            specialCase = true;
+            stringConstant += word + " ";
+            return;
+        }
+
+        if (separator.charAt(0) == '"' && isStringLexicallyCorrect) {
+            specialCase = true;
+            isStringLexicallyCorrect = false;
+            stringConstant += word + " ";
+            return;
+        }
+
+        // Char part
+        if (word.contains("'") && !isCharLexicallyCorrect) {
+            specialCase = true;
+
+            this.isCharLexicallyCorrect = true;
+            if (word.charAt(word.length() - 1) == ';') {
+                String newWord = word.substring(0, word.length() - 1);
+                charConstant += newWord + " ";
+                detectedTokens.add(new Pair<String, Integer>(charConstant, currentLine));
+                detectedTokens.add(new Pair<String, Integer>(";", currentLine));
+                charConstant = "";
+                return;
+            } else {
+                charConstant += word + " ";
+                detectedTokens.add(new Pair<String, Integer>(charConstant, currentLine));
+                charConstant = "";
+                return;
+            }
+        }
+
+        if (!isCharLexicallyCorrect) {
+            specialCase = true;
+            charConstant += word + " ";
+            return;
+        }
+
+        if (separator.equals("'") && isCharLexicallyCorrect) {
+            specialCase = true;
+            isCharLexicallyCorrect = false;
+            charConstant += word + " ";
+            return;
+        }
+
+        if (separator.equals("(")) {
+            specialCase = true;
+            String[] RHS;
+            String[] LHS;
+            for (String specialSeparator : this.specialRelational) {
+                if (word.contains(specialSeparator)) {
+                    containsRelational = true;
+                    splitList = word.split(specialSeparator);
+                    RHS = splitList[0].split("\\(");
+                    LHS = splitList[1].split("\\)");
+                    detectedTokens.add(new Pair<String, Integer>("(", currentLine));
+                    detectedTokens.add(new Pair<String, Integer>(RHS[1], currentLine));
+                    detectedTokens.add(new Pair<String, Integer>(specialSeparator, currentLine));
+                    detectedTokens.add(new Pair<String, Integer>(LHS[0], currentLine));
+                    detectedTokens.add(new Pair<String, Integer>(")", currentLine));
+                }
+            }
+            for (String regularSeparator : this.regularRelational) {
+                if (word.contains(regularSeparator) && !containsRelational) {
+                    containsRelational = true;
+                    splitList = word.split(regularSeparator);
+                    RHS = splitList[0].split("\\(");
+                    LHS = splitList[1].split("\\)");
+                    detectedTokens.add(new Pair<String, Integer>("(", currentLine));
+                    detectedTokens.add(new Pair<String, Integer>(RHS[1], currentLine));
+                    detectedTokens.add(new Pair<String, Integer>(regularSeparator, currentLine));
+                    detectedTokens.add(new Pair<String, Integer>(LHS[0], currentLine));
+                    detectedTokens.add(new Pair<String, Integer>(")", currentLine));
+                }
+            }
+            if (!containsRelational) {
+                splitList = word.split("\\(");
+                detectedTokens.add(new Pair<String, Integer>(separator, currentLine));
+                detectedTokens.add(new Pair<String, Integer>(splitList[1], currentLine));
+            }
+        }
+
+        if (separator.equals(")")) {
+            specialCase = true;
+            splitList = word.split("\\)");
+            detectedTokens.add(new Pair<String, Integer>(splitList[0], currentLine));
+            detectedTokens.add(new Pair<String, Integer>(separator, currentLine));
+        }
+
+        if (separator.equals("[")) {
+            specialCase = true;
+            splitList = word.split("\\[");
+            detectedTokens.add(new Pair<String, Integer>(splitList[0], currentLine));
+            detectedTokens.add(new Pair<String, Integer>(separator, currentLine));
+            String[] LHS = splitList[1].split("\\]");
+            if (LHS.length == 1) {
+                detectedTokens.add(new Pair<String, Integer>(LHS[0], currentLine));
+                detectedTokens.add(new Pair<String, Integer>("]", currentLine));
+            } else if (LHS.length == 2) {
+                detectedTokens.add(new Pair<String, Integer>(LHS[0], currentLine));
+                detectedTokens.add(new Pair<String, Integer>("]", currentLine));
+                detectedTokens.add(new Pair<String, Integer>(LHS[1], currentLine));
+            }
+        }
+
+        if (separator.equals("?")) {
+            detectedTokens.add(new Pair<String, Integer>(separator, currentLine));
+            specialCase = true;
+        }
+
+        if (separator.equals("+")) {
+            detectedTokens.add(new Pair<String, Integer>(separator, currentLine));
+            specialCase = true;
+        }
+
+        if (separator.equals(".")) {
+            splitList = word.split("\\.");
+            detectedTokens.add(new Pair<String, Integer>(splitList[0], currentLine));
+            detectedTokens.add(new Pair<String, Integer>(separator, currentLine));
+            specialCase = true;
+        }
+
+        if (!specialCase) {
+            splitList = word.split(separator);
+            if (splitList.length == 0) {
+                detectedTokens.add(new Pair<String, Integer>(separator, currentLine));
+            }
+
+            if (splitList.length == 1) {
+                detectedTokens.add(new Pair<String, Integer>(splitList[0], currentLine));
+                detectedTokens.add(new Pair<String, Integer>(separator, currentLine));
+            }
+
+            if (splitList.length == 2) {
+                if (!splitList[0].equals("")) {
+                    detectedTokens.add(new Pair<String, Integer>(splitList[0], currentLine));
+                }
+                detectedTokens.add(new Pair<String, Integer>(separator, currentLine));
+                detectedTokens.add(new Pair<String, Integer>(splitList[1], currentLine));
+            }
+        }
+    }
+}
